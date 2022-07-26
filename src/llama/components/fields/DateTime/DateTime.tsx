86b5dae56f9884291d688d36472cac9e0bd06430
sajal <sajal.mahapatra@nettantra.net>
2022-07-26 10:38:15 +0530

    optimized datatime field with added classname and styling

--- src/llama/components/fields/DateTime/DateTime.tsx
+++ src/llama/components/fields/DateTime/DateTime.tsx
@@ -13,23 +13,11 @@ interface Props {
 export default function DateTimeField(props: Props) {
     const { properties, handleData, name } = props
     const [error, setError] = useState(false)
-    let inputRef: any = useRef();
 
     const handleChange = (e: any) => {
         handleData(e.target.value, false)
     }
-    useEffect(() => {
-        if (properties?.["className"]?.trim()) {
-            inputRef.current.style = ""
-            inputRef.current.className = properties?.["className"] ?? name
-        }
-        if (properties["style"]) {
-            inputRef.current.style = ""
-            for (let key in properties["style"]) {
-                inputRef.current.style.setProperty(key, properties["style"][key]);
-            }
-        }
-    }, []);
+
     return (
         <>
             <h3 style={{ fontFamily: 'Nunito Sans', fontWeight: '400', fontSize: '16px', margin: '5px 0' }}>{properties['label']}</h3>
@@ -37,20 +25,20 @@ export default function DateTimeField(props: Props) {
                 id={name}
                 name={name}
                 type='datetime-local'
+                className={properties?.['className'] ?? "llama-datetime"}
                 value={props.parentState[name]?.value}
-                disabled={properties['readOnly'] ? properties['readOnly'] : false}
-                required={properties['required'] ? properties['required'] : false}
-                autoFocus={properties['autoFocus'] ? properties['autoFocus'] : false}
+                disabled={properties?.['readOnly'] ?? false}
+                required={properties?.['required'] ?? false}
+                autoFocus={properties?.['autoFocus'] ?? false}
                 autoComplete={properties['autoComplete'] ? "on" : "off"}
-                height={properties['height'] ? properties['height'] : null}
-                width={properties['width'] ? properties['width'] : null}
-                style={{ width: '95%', padding: '7px', border: '1px solid #000', borderRadius: '5px', fontSize: '14px', fontFamily: 'Nunito Sans', fontWeight: '400' }}
-                onChange={(e) => { handleChange(e) }}
-                ref={inputRef}
+                height={properties?.['height'] ?? null}
+                width={properties?.['width'] ?? null}
+                style={properties?.['style'] ?? { width: '95%', padding: '7px', border: '1px solid #000', borderRadius: '5px', fontSize: '14px', fontFamily: 'Nunito Sans', fontWeight: '400' }}
+                onChange={(e) => handleChange(e)}
             />
             <div style={{ marginBottom: '20px' }}>
                 <p style={{ margin: '5px 0px', fontFamily: 'Nunito Sans', fontWeight: '200', fontSize: '14px' }}>{properties['description']}</p>
-                {error ? <p style={{ marginTop: '5px', fontFamily: 'Nunito Sans', fontWeight: '600', fontSize: '14px', color: '#9e001a' }}>{properties['errorMessage'] ? properties['errorMessage'] : `Something went wrong in ${name} field`}</p> : null}
+                {error ? <p style={{ marginTop: '5px', fontFamily: 'Nunito Sans', fontWeight: '600', fontSize: '14px', color: '#9e001a' }}>{properties?.['errorMessage'] ?? `Something went wrong in ${name} field`}</p> : null}
             </div>
         </>
     )
