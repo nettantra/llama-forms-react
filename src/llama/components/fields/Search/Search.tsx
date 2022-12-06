eea55870dfc7047e0a846d4f2f484a0acf9da3ed
sajal.mahapatra <sajal.mahapatra@nettantra.net>
2022-12-06 09:17:54 +0530

    added type declaration and label in search field

--- src/llama/components/fields/Search/Search.tsx
+++ src/llama/components/fields/Search/Search.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SearchField = () => {
-  return (
-    <>
-    <input type="search"/>
-    </>
-  )
+interface LooseObject {
+  [key: string]: any;
 }
+interface Props {
+  properties: LooseObject;
+  handleData: any;
+  name: any;
+  parentState: any;
+}
+const SearchField = (props: Props) => {
+  const { properties, handleData, name } = props;
+  const [error, setError] = useState(false);
 
-export default SearchField
+  return (
+    <div>
+      <h3
+        style={{
+          fontFamily: "Nunito Sans",
+          fontWeight: "400",
+          fontSize: "16px",
+          margin: "5px 0",
+        }}
+      >
+        {properties["label"]}
+      </h3>
+      <input type="search" />
+    </div>
+  );
+};
+
+export default SearchField;
